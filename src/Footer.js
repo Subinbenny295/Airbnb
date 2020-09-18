c2d5d15c600c89ffe9363fa6dbad516452e991dc
Subin B Thomas <Subinbthomas5@gmail.com>
2020-09-18 11:58:52 +0530

    final

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>🅒 2020 Airbnb clone! No Rights Reserved - This is a demo</p>
+            <p>Privacy • Terms • Sitemap • Comapny Details</p>
+        </div>
+    )
+}
+
+export default Footer
